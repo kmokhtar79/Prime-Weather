738c225e8274c261b153272b993dce3cd1ca89a1
Kamra Mokhtar <kmokhtar79@yahoo.com>
2021-02-19 22:48:14 +0800

    Update Prime-widget.js

--- Prime-widget.js
+++ Prime-widget.js
@@ -598,87 +598,87 @@ function cropImage(img, rect) {
 function phoneSizes() {
   let phones = {
    "2532": {
+    "models"  : ["12", "12 Pro"],
+    "small"   : {"w": 474, "h": 474 },
+    "medium"  : {"w": 1014, "h": 474 },
+    "large"   : {"w": 1014, "h": 1062 },
     "left"    : 78,
     "right"   : 618,
     "top"     : 231,
     "middle"  : 819,
     "bottom"  : 1407
   },
-    "2688": {
-      "small": 507,
-      "medium": 1080,
-      "large": 1137,
-      "left": 81,
-      "right": 654,
-      "top": 228,
-      "middle": 858,
-      "bottom": 1488
-    },
+   
+  "2688": {
+    "models"  : ["Xs Max", "11 Pro Max"],
+    "small"   : {"w": 507,  "h": 507},
+    "medium"  : {"w": 1080, "h": 507},
+    "large"   : {"w": 1080, "h": 1137},
+    "left"    : 81,
+    "right"   : 654,
+    "top"     : 228,
+    "middle"  : 858,
+    "bottom"  : 1488
+  },
+  
+  "1792": {
+    "models"  : ["11", "Xr"],
+    "small"   : {"w": 338, "h": 338},
+    "medium"  : {"w": 720, "h": 338},
+    "large"   : {"w": 720, "h": 758},
+    "left"    : 54,
+    "right"   : 436,
+    "top"     : 160,
+    "middle"  : 580,
+    "bottom"  : 1000
+  },
+  
+  "2436": {
+    "models"  : ["X", "Xs", "11 Pro"],
+    "small"   : {"w": 465, "h": 465},
+    "medium"  : {"w": 987, "h": 465},
+    "large"   : {"w": 987, "h": 1035},
+    "left"    : 69,
+    "right"   : 591,
+    "top"     : 213,
+    "middle"  : 783,
+    "bottom"  : 1353
+  },
+  
+  "2208": {
+    "models"  : ["6+", "6s+", "7+", "8+"],
+    "small"   : {"w": 471, "h": 471},
+    "medium"  : {"w": 1044, "h": 471},
+    "large"   : {"w": 1044, "h": 1071},
+    "left"    : 99,
+    "right"   : 672,
+    "top"     : 114,
+    "middle"  : 696,
+    "bottom"  : 1278
+  },
+  
+  "1334": {
+    "models"  : ["6","6s","7","8"],
+    "small"   : {"w": 296, "h": 296},
+    "medium"  : {"w": 642, "h": 296},
+    "large"   : {"w": 642, "h": 648},
+    "left"    : 54,
+    "right"   : 400,
+    "top"     : 60,
+    "middle"  : 412,
+    "bottom"  : 764
+  },
 
-    "1792": {
-      "small": 338,
-      "medium": 720,
-      "large": 758,
-      "left": 54,
-      "right": 436,
-      "top": 160,
-      "middle": 580,
-      "bottom": 1000
-    },
-
-    "2436": {
-      "small": 465,
-      "medium": 987,
-      "large": 1035,
-      "left": 69,
-      "right": 591,
-      "top": 213,
-      "middle": 783,
-      "bottom": 1353
-    },
-
-    "2208": {
-      "small": 471,
-      "medium": 1044,
-      "large": 1071,
-      "left": 99,
-      "right": 672,
-      "top": 114,
-      "middle": 696,
-      "bottom": 1278
-    },
-
-    "1334": {
-      "small": 296,
-      "medium": 642,
-      "large": 648,
-      "left": 54,
-      "right": 400,
-      "top": 60,
-      "middle": 412,
-      "bottom": 764
-    },
-
-    "1136": {
-      "small": 282,
-      "medium": 584,
-      "large": 622,
-      "left": 30,
-      "right": 332,
-      "top": 59,
-      "middle": 399,
-      "bottom": 399
-    },
-    
-    "1624": {
-      "small": 310,
-      "medium": 658,
-      "large": 690,
-      "left": 46,
-      "right": 394,
-      "top": 142,
-      "middle": 522,
-      "bottom": 902 
+  "1136": {
+    "models"  : ["5","5s","5c","SE"],
+    "small"   : {"w": 282, "h": 282},
+    "medium"  : {"w": 584, "h": 282},
+    "large"   : {"w": 584, "h": 622},
+    "left"    : 30,
+    "right"   : 332,
+    "top"     : 59,
+    "middle"  : 399,
+    "bottom"  : 399 
     }
   }
   return phones
